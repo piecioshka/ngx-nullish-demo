49393499b7a2ea556155d3d0b7620f8ab4497ca1
Piotr Kowalski <piecioshka@gmail.com>
2020-06-21 02:03:23 +0200

    Use ngx-nullish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+import { Subject } from 'rxjs';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ngx-nullish-demo';
+  numbers$ = new Subject();
+
+  send(v: any) {
+    this.numbers$.next(v);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgxNullishModule } from 'ngx-nullish';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxNullishModule
   ],
   providers: [],
   bootstrap: [AppComponent]
